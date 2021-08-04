1059f6b41d36f8c35e5b26f3871f139a173a9b02
Joke Heyndels <jo_hannah@outlook.com>
2021-08-04 12:06:59 +0000

    Display tasks in collapsible

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -14,4 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     let select = document.querySelectorAll('select');
     M.FormSelect.init(select);
+
+    let collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
   });
