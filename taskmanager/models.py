fd25eb0fb27b59d713e553ce66acbf12ee5bdc4b
Joke Heyndels <jo_hannah@outlook.com>
2021-10-01 10:22:56 +0000

    Add User model for auth demo

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,6 +1,13 @@
 from taskmanager import db
 
 
+class User(db.Model):
+    # schema for the User model
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password =  db.Column(db.String(25), nullable=False)
+
+
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
@@ -29,7 +36,13 @@ class Task(db.Model):
             "category.id", ondelete="CASCADE"
         ), 
         nullable=False)
+    owner_id = db.Column(
+        db.Integer,
+        db.ForeignKey(
+            "user.id", ondelete="CASCADE"
+        )
+    )
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return f'#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}'
+        return f'#{self.id} Task: {self.task_name} | Urgent: {self.is_urgent}'
