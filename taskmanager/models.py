0954c49ba50398cc6b69e89c5a43129b937165c6
Joke Heyndels <jo_hannah@outlook.com>
2021-10-01 12:08:17 +0000

    Add user model for Authentication demo

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,7 +5,17 @@ class User(db.Model):
     # schema for the User model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password =  db.Column(db.String(25), nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    tasks = db.relationship(
+        "Task",
+        backref="user",
+        cascade="all, delete",
+        lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.username
 
 
 class Category(db.Model):
@@ -13,9 +23,9 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship(
-        "Task", 
-        backref="category", 
-        cascade="all, delete", 
+        "Task",
+        backref="category",
+        cascade="all, delete",
         lazy=True)
 
     def __repr__(self):
@@ -31,12 +41,12 @@ class Task(db.Model):
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(
-        db.Integer, 
+        db.Integer,
         db.ForeignKey(
             "category.id", ondelete="CASCADE"
         ), 
         nullable=False)
-    owner_id = db.Column(
+    task_owner_id = db.Column(
         db.Integer,
         db.ForeignKey(
             "user.id", ondelete="CASCADE"
